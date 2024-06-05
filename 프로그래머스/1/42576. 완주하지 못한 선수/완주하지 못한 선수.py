30cde57a4aaa2d862fcb53920c4366d6f99f02b1
futuremirae <136614563+futuremirae@users.noreply.github.com>
2024-06-05 15:51:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.42 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,20 +1,15 @@
 def solution(participant, completion):
+    hashmap = {}
+    sumhash = 0
+    # 키와 밸류 저장 완료 
+    for part in participant:
+        hashmap[hash(part)] = part # 키와 밸류 값으로 저장 
+        sumhash += hash(part)
+    for com in completion: 
+        sumhash -= hash(com)
+    return hashmap[sumhash]
     
-    answer = ''
-
-    # 1. 두 list를 sorting한다
-    participant.sort()
-    completion.sort()
-
-    # 2. completeion list의 len만큼 participant를 찾아서 없는 사람을 찾는다
-    for i in range(len(completion)):
-        if(participant[i] != completion[i]):
-            return participant[i]
-
-    # 3. 전부 다 돌아도 없을 경우에는 마지막 주자가 완주하지 못한 선수이다.
-    return participant[-1]
-
-
+        
 
     
    
