afbc0413748b1368bd1532803b7147931eacee79
futuremirae <136614563+futuremirae@users.noreply.github.com>
2024-07-11 17:44:27 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.swift
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.swift
@@ -1,23 +1,14 @@
 import Foundation
 
 func solution(_ hp:Int) -> Int {
-    
-    var hp = hp
-    var result = 0
-    while hp != 0 {
-        if hp >= 5 {
-            result += hp / 5
-            hp = hp % 5
-        }
-        
-        if hp >= 3 {
-            result += hp / 3
-            hp = hp % 3
-        }
-        
-        result += hp 
-        break            
-       
-    }
-    return result
+	var hp = hp
+	var result = 0
+	for i in [5,3,1] {
+		result += hp / i
+        hp %= i
+	
+		
+	}
+	
+	return result
 }
