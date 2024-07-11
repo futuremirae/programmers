70c7bf31b0746ab94fc7dd76bd93f05bb108360b
futuremirae <136614563+futuremirae@users.noreply.github.com>
2024-07-11 18:48:19 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 func solution(_ my_string:String) -> Int {
-    var my_string = my_string.filter { ["0","1","2","3","4","5","6","7","8","9"].contains($0) }
+    // var my_string = my_string.filter { ["0","1","2","3","4","5","6","7","8","9"].contains($0) }
     var result = 0
    for chr in my_string {
-       result += Int(String(chr))!
+       result += Int(String(chr)) ?? 0
    }
     
     
