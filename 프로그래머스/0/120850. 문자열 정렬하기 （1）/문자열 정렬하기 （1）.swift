0bc948c0f4b0384e94f13106c8fcf137167e6925
futuremirae <136614563+futuremirae@users.noreply.github.com>
2024-07-12 14:31:40 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.27 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ my_string:String) -> [Int] {
+    let my_string = my_string.filter { $0.isNumber } // string 
+    let result = my_string.compactMap { Int(String($0)) } // character이므로 int 적용불가 
+   
+    return result.sorted()
+}
